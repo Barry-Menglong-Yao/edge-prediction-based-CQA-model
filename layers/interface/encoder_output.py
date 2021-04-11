ed73e6c7abcd45c52cff0c2c8c8541878dd647a9
Barry <dragonbrightfuture@gmail.com>
2021-04-10 21:01:59 -0400

    class

--- layers/interface/encoder_output.py
+++ layers/interface/encoder_output.py
@@ -5,23 +5,43 @@ Paragraph:
 	sentence_list
 		word_list
 			embedding
-			word_str
+			content
 			
 conversation_history:
 	question_list
-		sentence_list
-			word_list
+		word_list
 				embedding
-				word_str
+				content
 	answer_list
-		sentence_list
-			word_list
+		word_list
 				embedding
-				word_str
+				content
 			
 current question:
-	sentence_list
-		word_list
+	word_list
 			embedding
-			word_str
+			content
 """
+# maybe it is not a good idea to pass it by class since it is not convenient for original model to process.
+# Let us check it later.
+class Paragraph:
+	def __init__(self, sentence_list):
+		self.sentence_list = sentence_list 
+
+class Sentence:
+	def __init__(self, word_list):
+	    self.word_list = word_list 
+
+class Word:
+	def __init__(self, embedding, content):
+	    self.embedding = embedding 
+	    self.content=content   #content like 'Barry'. String of the word
+
+class ConversationHistory:
+	def __init__(self, question_list,answer_list):
+		self.question_list = question_list 
+		self.answer_list = answer_list 
+
+class CurrentQuestion:
+	def __init__(self, word_list ):
+		self.word_list = word_list 
